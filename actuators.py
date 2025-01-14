ff0ebe7f0b3fd2e07f88552fce6eb574beb37b14
VascoII <fabriceds@yahoo.fr>
2025-01-14 20:34:25 +0100

    Create actuators.py

---
+++ actuators.py
@@ -0,0 +1,12 @@
+# actuators.py
+
+def open_windows():
+    print("👉 Ouverture des fenêtres (simulation)...")
+    # Ici, tu mettrais le code pour piloter le moteur d’ouverture
+
+def close_windows():
+    print("👉 Fermeture des fenêtres (simulation)...")
+    # Idem, pilotage du matériel
+
+def do_nothing():
+    print("Aucune action requise.")
